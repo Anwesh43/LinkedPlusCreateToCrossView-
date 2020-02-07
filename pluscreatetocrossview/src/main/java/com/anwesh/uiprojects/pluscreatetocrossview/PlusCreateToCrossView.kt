ec128a2ebc23522f8b9e49ae57adbb13f921c176
Anwesh43 <anweshthecool0@gmail.com>
2020-02-07 10:25:02 +0530

    adding constants and libraries

--- pluscreatetocrossview/src/main/java/com/anwesh/uiprojects/pluscreatetocrossview/PlusCreateToCrossView.kt
+++ pluscreatetocrossview/src/main/java/com/anwesh/uiprojects/pluscreatetocrossview/PlusCreateToCrossView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.pluscreatetocrossview
 /**
  * Created by anweshmishra on 07/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20

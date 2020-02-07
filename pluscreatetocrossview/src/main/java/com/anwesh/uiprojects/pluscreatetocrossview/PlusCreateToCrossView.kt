fdc4fd90a3045e8662f15ee229fb4e924825c3e9
Anwesh43 <anweshthecool0@gmail.com>
2020-02-07 10:41:55 +0530

    adding PlusCreateToCrossView class

--- pluscreatetocrossview/src/main/java/com/anwesh/uiprojects/pluscreatetocrossview/PlusCreateToCrossView.kt
+++ pluscreatetocrossview/src/main/java/com/anwesh/uiprojects/pluscreatetocrossview/PlusCreateToCrossView.kt
@@ -62,3 +62,21 @@ fun Canvas.drawPCCNode(scale : Float, i : Int, paint : Paint) {
     drawPlusCreateLineToCross(scale, size, paint)
     restore()
 }
+
+class PlusCreateToCrossView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
